456ea7d11f59d6640045cb73150c3e6a4295811d
Stuart Himmer <stuarthimmer@Stuarts-MacBook-Pro.local>
2021-12-08 16:00:53 -0700

    day 4 part 2

--- puzzle4go/main.go
+++ puzzle4go/main.go
@@ -10,9 +10,33 @@ import (
 
 func main() {
 	boards, entries := buildBoardsAndGetEntries()
-	winningBoard, lastCalledEntry := playBingo(boards, entries)
+	solvePart1(boards, entries)
+	solvePart2(boards, entries)
+}
+
+func solvePart2(boards []board, entries []string) {
+	winners := playBingo(boards, entries, false)
+	lastCall, err := strconv.Atoi(winners[len(winners)-1].lastCalledNum)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+	}
+	fmt.Println(lastCall)
+	fmt.Println("Part 2 Answer: ", calculateScore(winners[len(winners)-1].Board, lastCall))
+}
+
+func solvePart1(boards []board, entries []string) {
+	winners := playBingo(boards, entries, true)
+	lastCall, err := strconv.Atoi(winners[0].lastCalledNum)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+	}
+
+	fmt.Println("Part 1 Answer: ", calculateScore(winners[0].Board, lastCall))
+}
+
+func calculateScore(b board, lastCall int) int {
 	sum := 0
-	for _, r := range winningBoard.rows {
+	for _, r := range b.rows {
 		for _, c := range r {
 			if !c.selected {
 				value, err := strconv.Atoi(c.value)
@@ -23,27 +47,43 @@ func main() {
 			}
 		}
 	}
-	lastCall, err := strconv.Atoi(lastCalledEntry)
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-	}
-	fmt.Println("Answer: ", sum*lastCall)
 
+	return sum * lastCall
 }
 
-func playBingo(boards []board, entries []string) (board, string) {
-	winner := board{}
-	for i, e := range entries {
+type winner struct {
+	id            int
+	Board         board
+	lastCalledNum string
+}
+
+func winnersContain(winners []winner, id int) bool {
+	result := false
+	for _, w := range winners {
+		if w.id == id {
+			result = true
+			break
+		}
+	}
+	return result
+}
+
+func playBingo(boards []board, entries []string, oneWinner bool) []winner {
+	winners := []winner{}
+	for _, e := range entries {
 		for b := range boards {
-			boards[b].selectCells(e)
-			if i >= 4 {
+			if !winnersContain(winners, b) {
+				boards[b].selectCells(e)
 				if boards[b].isWinner() {
-					return boards[b], e
+					winners = append(winners, winner{id: b, Board: boards[b], lastCalledNum: e})
+					if oneWinner {
+						return winners
+					}
 				}
 			}
 		}
 	}
-	return winner, ""
+	return winners
 }
 
 func parseFile() string {

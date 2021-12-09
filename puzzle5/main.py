08318d4b4091df0b12792dfac722d9e3d1ce2f71
Stuart Himmer <stuarthimmer@Stuarts-MacBook-Pro.local>
2021-12-09 16:21:21 -0700

    clean up commented out lines and added more type hinting

--- puzzle5/main.py
+++ puzzle5/main.py
@@ -1,21 +1,15 @@
 import pprint
 
-def drawStraightLine(start: list, end: list, map: list):
+def drawStraightLine(start: list, end: list, map: list) -> list:
  
     y = start[1] if start[1] == end[1] else False
 
     if y or type(y) == type(0):
-
-        # print("drawing line of row ", y)
         lowestX = start[0] if start[0] < end[0] else end[0]
         highestX = start[0] if start[0] > end[0] else end[0]
-        # print(lowestX, highestX)
         if len(map) < y:
-            # print(f"map has {len(map)} rows, adding {y+1} rows")
             map = map + [[0] * (highestX+1) for i in range(y+1)]
-            # print(f"map now has {len(map)} rows")
         if len(map[y]) < highestX+1:
-            # print(f"adding {highestX - len(map[y])} columns to row {y}")
             map[y] = map[y] + [0] * (highestX - len(map[y]) + 1)
         for x in range(lowestX, highestX+1):
             map[y][x] = map[y][x] + 1
@@ -23,41 +17,29 @@ def drawStraightLine(start: list, end: list, map: list):
     x = start[0] if start[0] == end[0] else False
     
     if x or type(x) == type(0):
-        # print("drawing line of column ", x)
         lowestY = start[1] if start[1] < end[1] else end[1]
         highestY = start[1] if start[1] > end[1] else end[1]
         for y in range(lowestY, highestY + 1):
-            # print(x, y)
-            # print(len(map))
             if len(map) < y+1:
-                # print(f"map has {len(map)} rows, adding {y+1 - len(map)} rows")
                 map = map + [[0] * (x+1) for i in range(y+1 - len(map))]
-                # print(f"map now has {len(map)} rows")
             if len(map[y]) < x+1:    
                 map[y] = map[y] + [0] * (x - len(map[y]) + 1)
                 
-            # print(x,y)
-            # print(len(map[y]), len(map))
             map[y][x] = map[y][x] + 1
 
     return map
 
-def drawDiagnolLine(start: list, end: list, map: list):
+def drawDiagnolLine(start: list, end: list, map: list) -> list:
     minY = start[1] if start[1] < end[1] else end[1]
     maxX = start[0] if start[0] > end[0] else end[0]
     maxY = start[1] if start[1] > end[1] else end[1]
     diffY = maxY - minY
-    # print(diffX, diffY)
     if len(map) < maxY+1:
-        # print(f"map has {len(map)} rows, adding {y+1} rows")
         map = map + [[0] * (maxX+1) for i in range(maxY)]
-        # print(f"map now has {len(map)} rows")
 
     x = start[0]
     y = start[1]
-    # print(x,y)
     for _ in range(diffY+1):
-        # print("draw")
         if len(map[y]) < maxX+1:    
             map[y] = map[y] + [0] * (maxX - len(map[y]) + 1)
         map[y][x] = map[y][x] + 1
@@ -73,9 +55,8 @@ def drawDiagnolLine(start: list, end: list, map: list):
     return map
             
 
-def part1(map, thermalLines):
+def part1(map: list[list[int]], thermalLines: list[str]) -> int:
     for tl in thermalLines:
-        # print(tl)
         coords = tl.split(' -> ')
         start = coords[0].split(',')
         end = coords[1].split(',')
@@ -86,8 +67,6 @@ def part1(map, thermalLines):
         if start[0] == end[0] or start[1] == end[1]:
             
             map = drawStraightLine(start, end, map)
-        
-    
     count = 0
     for y in map:
         for x in y:
@@ -95,8 +74,7 @@ def part1(map, thermalLines):
                 count = count+1
     return count
 
-
-def part2(map, thermalLines):
+def part2(map: list[list[int]], thermalLines: list[str]) -> int:
     for tl in thermalLines:
         # print(tl)
         coords = tl.split(' -> ')
